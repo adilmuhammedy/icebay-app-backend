6c89ad56a9ebe95768704e5247ca63be8070ac6d
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2025-04-09 22:47:38 +0530

    added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5433,
-      password: 'Password@123',
-      username: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      password: process.env.DATABASE_PASSWORD || 'password',
+      username: process.env.DATABASE_USER || 'postgres',
       entities: [],
-      database: 'icebay',
+      database: process.env.DATABASE_NAME || 'icebay',
       synchronize: true,
       logging: true,
     }),
